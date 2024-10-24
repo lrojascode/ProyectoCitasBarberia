dd7178bd4ab80af25864777b112383a494cf2c5a
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-10-23 19:10:25 -0500

    Feat: fetch profesionals from database

--- FrontEnd/barberia-app17/src/app/site/pages/profesionals/profesionals.component.ts
+++ FrontEnd/barberia-app17/src/app/site/pages/profesionals/profesionals.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ProfesionalsStore } from '../../../features/profesionals/profesionals.store';
+import { Profesional } from '../../../features/profesionals/models/profesional.interface';
 
 @Component({
   selector: 'app-profesionals',
@@ -9,4 +11,8 @@ import { RouterLink } from '@angular/router';
   templateUrl: './profesionals.component.html',
   styles: ``,
 })
-export class ProfesionalsComponent {}
+export class ProfesionalsComponent {
+  private readonly profesionalsStore = inject(ProfesionalsStore);
+
+  public profesionals: Signal<Profesional[]> = this.profesionalsStore.entities;
+}
