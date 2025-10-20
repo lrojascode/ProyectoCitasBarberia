48a44124fb5505ea732d03e51c948617b749fe0e
Luis Rojas <125217566+lrojascode@users.noreply.github.com>
2025-10-20 15:27:20 -0500

    Update environment settings for production

--- FrontEnd/barberia-app17/src/environments/environment.ts
+++ FrontEnd/barberia-app17/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8190/api',
-  url: 'http://localhost:8190',
+  production: true,
+  apiUrl: 'https://proyectocitasbarberia.onrender.com',
 };
+
